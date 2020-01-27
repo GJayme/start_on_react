1465f866a4b44023c5de8a1499d4227abfa5e1b8
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-01-27 14:11:07 -0300

    Criação das rotas

--- index.js
+++ index.js
@@ -2,10 +2,47 @@ const express =  require('express')
 
 const server = express();
 
-// localhost:30000/teste
+server.use(express.json());
 
-server.get('/teste', (req, res) => {
-    return res.json({ message: 'Hello Word' });
+const users = ['Diego', 'Cláudio', 'Victor'];
+
+// Rota de mostrar todos os usuários:
+server.get('/users', (req, res) => {
+    return res.json(users);
 })
+// Rota de mostrar um único usuário por meio do seu index:
+server.get('/users/:index', (req, res) => {
+    const { index }  = req.params; 
+    return res.json(users[index]);
+})
+// Rota de criação de usuários:
+server.post('/users', (req,res) => {
+    const { name } = req.body;
 
-server.listen(3000);
+    users.push(name);
+
+    return res.json(users);
+});
+
+// Rota para editar algum usuário:
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
+});
+
+// Rota para deletar algum usuário:
+server.delete('/users/:index', (req,res) => {
+    const { index } = req.params;
+
+    users.splice(index, 1);
+
+    return res.send();
+});
+
+
+
+server.listen(3000); 
