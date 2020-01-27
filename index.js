6e62796b6fe5570c5a1f3cfd2d1c390fd296339e
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-01-27 14:53:40 -0300

    Criação dos Middlewares de verificação

--- index.js
+++ index.js
@@ -6,17 +6,46 @@ server.use(express.json());
 
 const users = ['Diego', 'Cláudio', 'Victor'];
 
+// Middleware Global - Middlewar de log da nossa aplicação:
+server.use((req, res, next) => {
+    console.time('Request');
+    console.log(`Método: ${req.method}; URL: ${req.url};`);
+
+    next();
+
+    console.timeEnd('Request');
+});
+
+// Middleware Local - Ele vai la no corpo da minha requisição e vai procurar se existe a informação name, caso nao encontre ele vai retornar um erro para o usuário com a mensagem.
+// Caso nao entra nesse IF ele vai chamar o Middleware da rota normalmente.
+function checkUserExists(req, res, next) {
+    if(!req.body.name){
+        return res.status(400).json({ error: 'User name is required' });
+    }
+
+    return next();
+}
+
+// Middleware Local - Ele vai fazer a checagem para ver se o usuário está cadastrado dentro do nosso Array de users.
+function checkUserInArray(req, res, next) {
+    if (!users[req.params.index]) {
+        return res.status(400).json({ error: 'User does not exists' });
+    }
+
+    return next();
+}
+
 // Rota de mostrar todos os usuários:
 server.get('/users', (req, res) => {
     return res.json(users);
 })
 // Rota de mostrar um único usuário por meio do seu index:
-server.get('/users/:index', (req, res) => {
+server.get('/users/:index', checkUserInArray, (req, res) => {
     const { index }  = req.params; 
     return res.json(users[index]);
 })
 // Rota de criação de usuários:
-server.post('/users', (req,res) => {
+server.post('/users', checkUserExists, (req,res) => {
     const { name } = req.body;
 
     users.push(name);
@@ -25,7 +54,7 @@ server.post('/users', (req,res) => {
 });
 
 // Rota para editar algum usuário:
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
     const { index } = req.params;
     const { name } = req.body;
 
@@ -35,7 +64,7 @@ server.put('/users/:index', (req, res) => {
 });
 
 // Rota para deletar algum usuário:
-server.delete('/users/:index', (req,res) => {
+server.delete('/users/:index', checkUserInArray, (req,res) => {
     const { index } = req.params;
 
     users.splice(index, 1);
