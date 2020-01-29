8b77ceb062b4c6594325fb513f936c808a33b912
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-01-28 21:49:22 -0300

    Finalização dos Middleware

--- index.js
+++ index.js
@@ -1,77 +1,80 @@
-const express =  require('express')
+const express = require("express");
 
 const server = express();
 
 server.use(express.json());
 
-const users = ['Diego', 'Cláudio', 'Victor'];
+const users = ["Diego", "Cláudio", "Victor"];
 
 // Middleware Global - Middlewar de log da nossa aplicação:
 server.use((req, res, next) => {
-    console.time('Request');
-    console.log(`Método: ${req.method}; URL: ${req.url};`);
+  console.time("Request");
+  console.log(`Método: ${req.method}; URL: ${req.url};`);
 
-    next();
+  next();
 
-    console.timeEnd('Request');
+  console.timeEnd("Request");
 });
 
 // Middleware Local - Ele vai la no corpo da minha requisição e vai procurar se existe a informação name, caso nao encontre ele vai retornar um erro para o usuário com a mensagem.
 // Caso nao entra nesse IF ele vai chamar o Middleware da rota normalmente.
 function checkUserExists(req, res, next) {
-    if(!req.body.name){
-        return res.status(400).json({ error: 'User name is required' });
-    }
+  if (!req.body.name) {
+    return res.status(400).json({ error: "User name is required" });
+  }
 
-    return next();
+  return next();
 }
 
 // Middleware Local - Ele vai fazer a checagem para ver se o usuário está cadastrado dentro do nosso Array de users.
 function checkUserInArray(req, res, next) {
-    if (!users[req.params.index]) {
-        return res.status(400).json({ error: 'User does not exists' });
-    }
+  const user = users[req.params.index];
 
-    return next();
+  if (!users[req.params.index]) {
+    return res.status(400).json({ error: "User does not exists" });
+  }
+
+  req.user = user;
+
+  return next();
 }
 
 // Rota de mostrar todos os usuários:
-server.get('/users', (req, res) => {
-    return res.json(users);
-})
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
 // Rota de mostrar um único usuário por meio do seu index:
-server.get('/users/:index', checkUserInArray, (req, res) => {
-    const { index }  = req.params; 
-    return res.json(users[index]);
-})
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  return res.json(req.user);
+});
+
 // Rota de criação de usuários:
-server.post('/users', checkUserExists, (req,res) => {
-    const { name } = req.body;
+server.post("/users", checkUserExists, (req, res) => {
+  const { name } = req.body;
 
-    users.push(name);
+  users.push(name);
 
-    return res.json(users);
+  return res.json(users);
 });
 
 // Rota para editar algum usuário:
-server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
-    const { index } = req.params;
-    const { name } = req.body;
+server.put("/users/:index", checkUserInArray, checkUserExists, (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
 
-    users[index] = name;
+  users[index] = name;
 
-    return res.json(users);
+  return res.json(users);
 });
 
 // Rota para deletar algum usuário:
-server.delete('/users/:index', checkUserInArray, (req,res) => {
-    const { index } = req.params;
+server.delete("/users/:index", checkUserInArray, (req, res) => {
+  const { index } = req.params;
 
-    users.splice(index, 1);
+  users.splice(index, 1);
 
-    return res.send();
+  return res.send();
 });
 
-
-
-server.listen(3000); 
+server.listen(3000);
